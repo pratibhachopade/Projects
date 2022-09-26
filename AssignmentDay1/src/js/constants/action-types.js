859d7b8229a12e176fe50fea47fa7a0805a806f7
pratibha_chopade <pratibha.chopade@yash.com>
2022-09-26 18:13:37 +0530

    React Assignment

--- AssignmentDay1/src/js/constants/action-types.js
+++
@@ -1,7 +0,0 @@
-export const DATA_LOADED = "DATA_LOADED";
-export const INCREAMENT = "INCREAMENT";
-export const DECREAMENT = "DECREAMENT";
-export const RESET = "RESET";
-export const ITEMS_LOADING = "ITEMS_LOADING";
-export const ADD_ARTICLE = "ADD_ARTICLE"
-
