506ea4b7182ef5f68da15115a152d9547f6d830c
pratibha_chopade <pratibha.chopade@yash.com>
2022-10-04 17:24:09 +0530

    Shopping_cart using React-Redux

--- Shopping_cart/src/App.js
+++ Shopping_cart/src/App.js
@@ -1,21 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./components/Header";
-import {Routes,Route} from "react-router-dom"
-import Cards from './components/Cards';
+import Header from './components/Header';
 import CardsDetails from './components/CardsDetails';
+import Cards from './components/Cards';
+import {Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <Header></Header>
-<Routes>
-  <Route path="/" element={<Cards/>} />
-   <Route path="/cart" element={<CardsDetails/>}/>
- 
-</Routes>
-    </div>
+  <>
+   <Header />
+   <Routes>
+     <Route path='/' element={<Cards />} />
+     <Route path='/cart/:id' element={<CardsDetails />} />
+   </Routes>
+  </>
   );
 }
 
